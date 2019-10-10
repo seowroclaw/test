63ae9fd53a110123783ca2a4c6aed050fb400dd5
Pawel Szewczyk <seo.wroclaw@gmail.com>
2019-10-10 12:06:47 +0200

    feature_1

--- src/Test.java
+++ src/Test.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
-
 public class Test {
+
+    String stringTest = "To string test";
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+    void Test() {
+
+        System.out.println("stringTest");
+    }
+
 }
